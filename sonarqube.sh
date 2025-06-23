134afcb16c081b0eb0b67316ed3737ad82916379
Rushikesh Mane <33669698+rushi2828@users.noreply.github.com>
2025-06-23 11:36:43 +0530

    Create sonarqube.sh

---
+++ sonarqube.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+cd /opt/
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
+unzip sonarqube-8.9.6.50800.zip
+amazon-linux-extras install java-openjdk11 -y
+useradd sonar
+chown sonar:sonar sonarqube-8.9.6.50800 -R
+chmod 777 sonarqube-8.9.6.50800 -R
+su - sonar
